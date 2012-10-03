d3d94481e2d31a3f213305dd806d89f3bb2a2a3e
Andy Davies <ad@andydavies.net>
2012-10-04 00:45:16 +0100

    Add unit test

---
+++ tests/wptunittest.php
@@ -0,0 +1,136 @@
+<?php
+
+class WPTTest extends PHPUnit_Framework_TestCase
+{
+
+	// Demo code to call WPT API, wait for a retrieve result
+	// Lots of code borrowed from HTTPArchive.org
+	
+	$wptAPIKey = "andysdemorocks";
+	$wptServer = "http://velocity.webpagetest.org/";
+	$location = "Europe";
+	
+	//$testURL = "http://news.bbc.co.uk";
+	$testURL = "http://andydavies.github.com/veudemo/";
+	
+	// Fetch file and retry if it fails.
+	function fetchUrl($fn) {
+		$contents = file_get_contents($fn);
+		return $contents;
+	}
+	
+	function submitTest() {
+		
+		global $wptAPIKey, $wptServer, $testURL, $location;
+		
+		$id = "";
+	
+		$request = $wptServer . 'runtest.php?f=xml&url=' . urlencode($testURL) . 
+			"&location=" . $location .
+			( $wptAPIKey ? "&k=" . $wptAPIKey : "" );
+	
+		$doc = new DOMDocument();
+		
+		if ( $doc ) {
+			$response = fetchUrl($request);
+			
+			if ( strlen($response) ) {
+				$doc->loadXML($response);
+				$nodes = $doc->getElementsByTagName('statusCode');
+				$code = (int)trim($nodes->item(0)->nodeValue);
+	
+				echo $code . "\n";
+	
+				if ( $code == 200 ) {
+					// Update status col in status table
+					$nodes = $doc->getElementsByTagName('testId');
+					$id = trim($nodes->item(0)->nodeValue);
+				} 
+			}
+			unset( $doc );
+		}
+		return $id;
+	}
+	
+	function getTestStatus($id) {
+		global $wptServer;
+		
+		$code = "";
+	
+		$request = $wptServer . 'testStatus.php?f=xml&test=' . urlencode($id);
+		
+		$doc = new DOMDocument();
+		
+		if ( $doc ) {
+			$response = fetchUrl($request);
+	
+			if ( strlen($response) ) {
+				$doc->loadXML($response);
+				$nodes = $doc->getElementsByTagName('statusCode');
+				$code = (int)trim($nodes->item(0)->nodeValue);
+			}
+			unset( $doc );
+		}
+		return $code;
+	}
+	
+	function getTestResult($id) {
+		global $wptServer;
+		
+		$code = "";
+		$scoreCompress = 0;
+		$fail = true;
+	
+		$request = $wptServer . 'xmlResult/' . urlencode($id) . "/";
+		
+		$doc = new DOMDocument();
+		
+		if ( $doc ) {
+			$response = fetchUrl($request);
+	
+			if ( strlen($response) ) {
+				$doc->loadXML($response);
+				$nodes = $doc->getElementsByTagName('statusCode');
+				$code = (int)trim($nodes->item(0)->nodeValue);
+				
+				if($code == 200) {
+					$nodes = $doc->getElementsByTagName('score_compress');
+					$scoreCompress = (int)trim($nodes->item(0)->nodeValue);
+					if ($scoreCompress < 75) {
+						$fail = true;
+						// throw the exception here
+					}
+					$this->assertGreaterThanOrEqual(75, score, "Make the images smaller");
+				}
+			}
+			unset( $doc );
+		}
+		return $fail;
+	}
+	
+	// submit test
+	$testID = submitTest();
+	
+	if(strlen($testID)) {
+		$status = 100;
+	
+		$wait = 10;
+		$maxWait = 100;
+	
+		while($status < 200 && $wait < $maxWait) {
+	
+			$status = getTestStatus($testID);
+	
+			if($status < 200) {
+				sleep($wait);
+				$wait *= 1.5;
+			}
+	
+		}
+	
+		if($status == 200) {
+			getTestResult($testID);
+		}
+}
+
+?>
